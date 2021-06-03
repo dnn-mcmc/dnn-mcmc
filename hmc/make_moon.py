ddae42625b70fc50f756fe98d61adab5a88c61bc
Matthew Engelhard <mme4@bstat-h2wf53t8.dhe.duke.edu>
2021-06-03 17:57:21 -0400

    updated make_moon.py

--- hmc/make_moon.py
+++ hmc/make_moon.py
@@ -6,7 +6,7 @@ import tensorflow_probability as tfp
 import numpy as np
 import random
 import time
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -30,6 +30,21 @@ def rerange(x, r = 6.0):
     out_of_range = tf.cast(tf.math.greater(tf.math.abs(x), r), tf.float32)
     sign = tf.math.sign(x)
     return x * (1 - out_of_range) + sign * r * out_of_range
+
+def cont_bern_log_norm(lam, l_lim=0.49, u_lim=0.51):
+    '''
+    computes the log normalizing constant of a continuous Bernoulli distribution in a numerically stable way.
+    returns the log normalizing constant for lam in (0, l_lim) U (u_lim, 1) and a Taylor approximation in
+    [l_lim, u_lim].
+    cut_y below might appear useless, but it is important to not evaluate log_norm near 0.5 as tf.where evaluates
+    both options, regardless of the value of the condition.
+    '''
+    
+    cut_lam = tf.where(tm.logical_or(tm.less(lam, l_lim), tm.greater(lam, u_lim)), lam, l_lim * tf.ones_like(lam))
+    log_norm = tm.log(tm.abs(2.0 * tm.atanh(1 - 2.0 * cut_lam))) - tm.log(tm.abs(1 - 2.0 * cut_lam))
+    taylor = tm.log(2.0) + 4.0 / 3.0 * tm.pow(lam - 0.5, 2) + 104.0 / 45.0 * tm.pow(lam - 0.5, 4)
+    return tf.where(tm.logical_or(tm.less(lam, l_lim), tm.greater(lam, u_lim)), log_norm, taylor)
+
 
 # MLP model
 class StochasticMLP(Model):
@@ -66,8 +81,12 @@ class StochasticMLP(Model):
         for i, (cv, pv, layer) in enumerate(
             zip(h_current, h_previous, self.fc_layers)):
             
+            logits = layer(pv)
+            
             ce = tf.nn.sigmoid_cross_entropy_with_logits(
-                labels=cv, logits=layer(pv))
+                labels=cv, logits=logits)
+            
+            ce += cont_bern_log_norm(tf.nn.sigmoid(logits))
             
             nlog_prob += tf.reduce_sum(ce, axis = -1)
         
@@ -87,8 +106,11 @@ class StochasticMLP(Model):
         for i, (cv, pv, layer) in enumerate(
             zip(h_current, h_previous, self.fc_layers)):
             
+            logits = layer(pv)
             ce = tf.nn.sigmoid_cross_entropy_with_logits(
-                labels=cv, logits=layer(pv))
+                labels=cv, logits=logits)
+            
+            ce += cont_bern_log_norm(tf.nn.sigmoid(logits))
             
             nlog_prob += tf.reduce_sum(ce, axis = -1)
         
@@ -109,7 +131,7 @@ class StochasticMLP(Model):
         return adaptive_hmc
     
     # new proposing-state method with HamiltonianMonteCarlo
-    def propose_new_state_hamiltonian(self, x, h, y, hmc_kernel):
+    def propose_new_state_hamiltonian(self, x, h, y, hmc_kernel, is_update_kernel = True):
     
         h_current = h
         h_current = [tf.cast(h_i, dtype=tf.float32) for h_i in h_current]
@@ -126,14 +148,19 @@ class StochasticMLP(Model):
             kernel = hmc_kernel,
             trace_fn = None,
             return_final_kernel_results = True)
+    
+        # Generate new states of chains
+        #h_state = rerange(samples[0][0])
+        h_state = samples[0][0]
+        h_new = tf.split(h_state, self.hidden_layer_sizes, axis = 1) 
         
-        new_step_size = samples[2].new_step_size.numpy()
-        ker_new = self.generate_hmc_kernel(x, y, new_step_size)
-        
-        h_state = rerange(samples[0][0])
-        h_new = tf.split(h_state, self.hidden_layer_sizes, axis = 1)   
-
-        return(h_new, ker_new)
+        # Update the kernel if necesssary
+        if is_update_kernel:
+            new_step_size = samples[2].new_step_size.numpy()
+            ker_new = self.generate_hmc_kernel(x, y, new_step_size)
+            return(h_new, ker_new)
+        else:
+            return h_new
     
     def update_weights(self, x, h, y, lr = 0.1):
         
@@ -149,18 +176,18 @@ class StochasticMLP(Model):
         logits = 0.0
         for layer in self.fc_layers:
             logits = layer(x)
-            x = tf.math.sigmoid(logits)
+            x = tm.sigmoid(logits)
         
         logits = self.output_layer(x)
-        probs = tf.math.sigmoid(logits)
+        probs = tm.sigmoid(logits)
         #print(probs)
-        labels = tf.cast(tf.math.greater(probs, 0.5), tf.int32)
+        labels = tf.cast(tm.greater(probs, 0.5), tf.int32)
 
         return labels
 
 # Make data
 np.random.seed(1234)
-X, Y = make_moons(500, noise = 0.1)
+X, Y = make_moons(1000, noise = 0.3)
 
 # Split into test and training data
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=73)
@@ -170,30 +197,54 @@ y_test = y_test.reshape((-1, 1))
 train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(32)
 test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
 
+
+print('Starting Standard Backprop')
+
+# Standard BP
+model_bp = keras.Sequential(
+    [
+        keras.Input(shape=(2,)),
+        layers.Dense(100, activation = "sigmoid"),
+        layers.Dense(1, activation = "sigmoid")
+    ]
+)
+
+import time
+batch_size = 32
+epochs = 1000
+opt = tf.keras.optimizers.SGD(learning_rate=1e-3)
+st = time.time()
+model_bp.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy", "AUC"])
+model_bp.fit(x_train, y_train, batch_size=batch_size, epochs=epochs)
+print(time.time() - st)
+
+print('Building CD-HMC Model')
+
 model = StochasticMLP(hidden_layer_sizes = [100], n_outputs=1)
 network = [model.call(x) for x, y in train_ds]
 kernels = [model.generate_hmc_kernel(x, y) for x, y in train_ds]
 
+print('Starting CD-HMC Burn-in')
+
 # Burn-in
-burnin = 10
+burnin = 500
 step_sizes = []
 for i in range(burnin):
     
-    #if(i % 100 == 0):
-    print("Step %d" % i)
-    network_new = []
-    kernels_new = []
+    if(i % 100 == 0):
+        print("Step %d" % i)
     
-    res = [model.propose_new_state_hamiltonian(x, net, y, ker) 
-               for (x, y), net, ker in zip(train_ds, network, kernels)]
-    
-    network_new, kernels_new = zip(*res)
-         
-    network = network_new
-    kernels = kernels_new
+    network, kernels = zip(*[
+        model.propose_new_state_hamiltonian(x, net, y, ker)
+        for (x, y), net, ker in zip(train_ds, network, kernels)
+    ])
+
+
+print('Starting CD-HMC')
 
 # Training
 epochs = 1000
+
 start_time = time.time()
 for epoch in range(epochs):
     
@@ -203,35 +254,13 @@ for epoch in range(epochs):
         
         # only one mini-batch
         model.update_weights(x, network[bs], y, 0.1)
-        
-        network_new = []
-
-        for (xx, yy), net, hmc_kernel in zip(train_ds, network, kernels):
-            net_current = net
-            net_current = [tf.cast(net_i, dtype=tf.float32) for net_i in net_current]
-            net_current = tf.concat(net_current, axis = 1)
-        
-            num_results = 1
-            num_burnin_steps = 0
-
-            samples = tfp.mcmc.sample_chain(
-                num_results = num_results,
-                num_burnin_steps = num_burnin_steps,
-                current_state = net_current, # may need to be reshaped
-                kernel = hmc_kernel,
-                trace_fn = None,
-                return_final_kernel_results = True)
-            
-            new_state = rerange(samples[0][0])
-            net_new = tf.split(new_state, [100], axis = 1)   
-            network_new.append(net_new)
-   
-        network = network_new
-        
+        res = [model.propose_new_state_hamiltonian(x, net, y, ker, is_update_kernel = False) \
+                   for (x, y), net, ker in zip(train_ds, network, kernels)]
+        network = res
         loss += -1 * tf.reduce_mean(model.target_log_prob(x, network[bs], y))
     
     preds = [model.get_predictions(images) for images, labels in train_ds]
     train_acc = accuracy_score(np.concatenate(preds), y_train)
     print("Epoch %d/%d: - %.4fs/step - loss: %.4f - accuracy: %.4f" 
           % (epoch + 1, epochs, (time.time() - start_time) / (epoch + 1), loss, train_acc))
-'''
+
